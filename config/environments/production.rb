d1022460c9780ffb8f8d046799d3257ee5592cdb
Yuzhe <yuzhe.c85@gmail.com>
2013-09-27 16:04:26 +0800

    modify configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,19 @@ Lots::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'lots.summers.com.tw' }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 	=> 587,
+    :domain               => 'lots.summers.com.tw',
+    :user_name            => 'adword@summers.com.tw',
+    :password             => '4957QW9780',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
